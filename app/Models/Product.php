a71994a07d8fbe5ed4785ec90377090292fe41d3
Mauricio Queiroz <ephiot@gmail.com>
2021-07-05 02:38:13 -0300

    Added Illuminate\Database\Eloquent\Collection to Product model

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use App\DTO\Product as ProductDTO;
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
